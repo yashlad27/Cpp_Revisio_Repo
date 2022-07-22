a5786e22f567956de95cd280b913949e19c99178
Yash Lad <yashlad727@gmail.com>
2022-07-22 20:55:26 +0530

    commit-07

---
+++ codefiles/if_else.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+/*
+        !TERNARY OPERATOR!
+*/
+int main(){
+    int time=20;
+    string result = (time<20) ? "Good day!" : "Good Eve!";
+    cout<<result;
+    return 0;
+}
