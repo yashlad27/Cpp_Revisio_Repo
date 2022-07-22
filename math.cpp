ab906222779f1154542f947cf421b9190c9daf78
Yash Lad <yashlad727@gmail.com>
2022-07-22 20:31:59 +0530

    commit-4

--- math.cpp
+++ math.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 int main(){
-    cout<<max(5,100);
-    cout<<min(5,100);
+    cout<<"Maximum Value: "<<max(5,100)<<endl;
+    cout<<"Minimum Value: "<<min(5,100)<<endl;
 
+    cout<<sqrt(6400)<<endl;
+    cout<<round(12341314142524.029809473278327021)<<endl;
+    cout<<log(2)<<endl;
+
+    cout<<abs(9.673)<<endl;
+    cout<<ceil(44.1)<<endl;
 
     return 0;
 }
