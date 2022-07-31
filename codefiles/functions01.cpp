d78e6f079def29904e95a8f4d64cec460f1708de
Yash Lad <yashlad727@gmail.com>
2022-07-31 19:18:38 +0530

    COMMIT-11

--- codefiles/functions01.cpp
+++ codefiles/functions01.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
 using namespace std;
 
+// void myFunction(){
+//     cout<<"myFunction executed!"<<endl;
+// }
+
+void myFunction();   // function declaration 
+void myStringFun(string fname);
+// void funcName(parameter1, parameter2, parameter3){
+
+// }
+
 int main(){
+    myFunction();
+
+    myStringFun("yash");
+    myStringFun("yash");
 
     return 0;
+}
+
+void myFunction() {
+    cout<<"myFunction executed!"<<endl;
+}
+
+void myStringFun(string fname){
+    cout<<fname<<"\n";
 }
