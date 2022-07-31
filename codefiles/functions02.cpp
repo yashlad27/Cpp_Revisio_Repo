9cefd0d712aab6954d791a7c793da40331db2968
Yash Lad <yashlad727@gmail.com>
2022-07-31 20:05:28 +0530

    commit-12

--- codefiles/functions02.cpp
+++ codefiles/functions02.cpp
@@ -5,10 +5,19 @@ void myFunction(string country = "norway"){
     cout<<country<<endl;
 }
 
+void myFunc1(string fname, int age){
+    cout<<fname<<"\n";
+    cout<<age<<"years old"<<endl;
+}
+
 int main(){
     myFunction("Sweden");
     myFunction("India");
     myFunction();
     myFunction("USA");
+
+    myFunc1("YASH", 21);
+    myFunc1("Yash", 22);
+    
     return 0;
 }
