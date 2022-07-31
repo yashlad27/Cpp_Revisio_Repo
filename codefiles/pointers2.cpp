efd555e261fbc3dfdf150f1fbc27b224ac896a44
Yash Lad <yashlad727@gmail.com>
2022-07-31 19:04:45 +0530

    commit-10

--- codefiles/pointers2.cpp
+++ codefiles/pointers2.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 int main(){
+    // C++ Dereference:
+    string food = "Pizza";
+    string* ptr = &food;
+
+    cout<<ptr<<"\n";
+    cout<<*ptr<<"\n";
+
+    // When used in declaration (string *ptr), it creates a pointer variable
+    // when not used in declaration, it acts as a dereference operator
+
+    cout<<food<<"\n";
+    cout<<&food<<endl;
+    cout<<*ptr<<endl;
     
+    *ptr = "Hamburger";
+    cout<<*ptr<<endl;
+    cout<<food<<"\n";
     return 0;
 }
