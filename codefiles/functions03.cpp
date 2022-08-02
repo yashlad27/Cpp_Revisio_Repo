3f98c3de531e36514b73765ab4ceeacf2ea1a15d
Yash Lad <yashlad727@gmail.com>
2022-08-02 22:52:33 +0530

    commit-15

--- codefiles/functions03.cpp
+++ codefiles/functions03.cpp
@@ -6,9 +6,16 @@ void myFun(string country ="Sweden"){
     cout<<country<<endl;
 }
 
+//MULTIPLE PARAMETERS
 void myFun1(string fname, int age){
     cout<<fname<<" is "<<age<<" years old."<<endl;
 }
+
+//Return values
+int myFunction(int x){
+    return 5+x;
+}
+
 int main(){
     myFun(); // no value given here
     myFun("India");
@@ -16,6 +23,9 @@ int main(){
 
     myFun1("Yash", 20);
     myFun1("Chhaya", 47);
+
+    cout<<myFunction(5)<<endl;
+    cout<<myFunction(2)<<endl;
 
     return 0;
 }
