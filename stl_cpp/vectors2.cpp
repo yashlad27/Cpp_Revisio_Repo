bdffd3098d0bbc66d5c6bde1132eac5474e15c63
Yash Lad <yashlad727@gmail.com>
2022-08-16 17:08:15 +0530

    vectors 2 file

--- stl_cpp/vectors2.cpp
+++ stl_cpp/vectors2.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 template <class T>
 void display(vector<T> &v){
+    cout<<"Displaying this vector: "<<endl;
     for(int i=0; i<v.size(); i++){
         cout<<v[i]<<" ";
     }
@@ -17,6 +18,10 @@ int main(){
     vector<char> vec3(4); // 4-element character vector
     vector<char> vec4(vec3); // 4-element character vector from vec3
     vector<int> v(6, 3); // 6-element vector of 3's
+    display(vec1);
+    display(vec2);
+    display(vec3);
+    display(v);
 
     int element, size=5;
 }
