c75264f625eb9a5762e21e7f7f68c51945c90a95
Yash Lad <yashlad727@gmail.com>
2022-08-17 10:24:04 +0530

    function objects

---
+++ stl_cpp/func_obj.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Function objects(FUNCTOR): function wrapped in a class so that it is available like an object
+    int arr[] = {18888,22,3,4222,51,6,7};
+    sort(arr, arr+6);
+    cout<<"Sorting in Ascending: ";
+    for(int i=0; i<6; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    sort(arr, arr+6, greater<int>());
+    cout<<"Sorting in Descending: ";
+    for(int i=0; i<6; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
