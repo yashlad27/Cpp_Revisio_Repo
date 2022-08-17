1fc2ab1b4e3ee5e611a7b8788a9038b7f02f1127
Yash Lad <yashlad727@gmail.com>
2022-08-17 14:26:00 +0530

    array traversal

--- dsa_cwh/arrays01.cpp
+++ dsa_cwh/arrays01.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main(){
+    // Operations on arrays:
+    // 1. Traversal 
+    // 2. Insertion
+    // 3. Deletion
+    // 4. Searching
 
+    int arr[] = {1,2,3,4};
+    for(int i=0; i < 4; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    // or
+    int size = sizeof(arr)/sizeof(int);
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
