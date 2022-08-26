286a94051eb2d01916f22cad5a03eb107fb01a13
Yash Lad <yashlad727@gmail.com>
2022-08-26 10:42:26 +0530

    recursion 01

--- cpp_udemy/recursion.cpp
+++ cpp_udemy/recursion.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+void countdownfrom(int num){
+    if(num>=0){
+        cout<<num<<endl;
+        countdownfrom(num-1);
+    }
+}
 
+int main(){
+    countdownfrom(10);
     return 0;
 }
