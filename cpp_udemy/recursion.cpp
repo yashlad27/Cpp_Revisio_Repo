9df12074ce238cac40ea8608e0501d9d99e7d97a
Yash Lad <yashlad727@gmail.com>
2022-08-26 10:52:15 +0530

    recursion 02

--- cpp_udemy/recursion.cpp
+++ cpp_udemy/recursion.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
 using namespace std;
 
+// Recursion: 
+
 void countdownfrom(int num){
     if(num>=0){
         cout<<num<<endl;
-        countdownfrom(num-1);
+        countdownfrom(num-1); 
+        // this is a frame and is called a call stack
+        // if repeated many times then stack overflow happens
+    }
+}
+
+int sumValues(int num){
+    if(num>=1){
+        return num + sumValues(num-1);
     }
 }
 
 int main(){
-    countdownfrom(10);
+    countdownfrom(10); // function call
+    int totalsum = sumValues(10);
+    cout<<"The sum is:" << totalsum << endl;
     return 0;
 }
