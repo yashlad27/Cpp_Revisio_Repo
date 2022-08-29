3805ceee220f77a5cba8d422d69b3a118dbae58b
Yash Lad <yashlad727@gmail.com>
2022-08-29 08:38:01 +0530

    functions added

--- cpp_udemy/tic_tac_toe.cpp
+++ cpp_udemy/tic_tac_toe.cpp
@@ -1,8 +1,20 @@
 #include<iostream>
+#include<string>
 using namespace std;
+
+const int ROWS = 3;
+const int COLS = 3;
+
+void runGame();
+void initialiseGameBoard(string gameBoard[ROWS][COLS]);
+void printCurrentBoard(string gameBoard[ROWS][COLS]);
+void getUserInput(bool xturn, string gameBoard[ROWS][COLS]);
+bool cellAlreadyOccupied(int row, int col, string gameBoard[ROWS][COLS]);
+string getWinner(string gameBoard[ROWS][COLS]);
+bool isBoardFull(string gameBoard[ROWS][COLS]);
 
 int main(){
     // TIC - TAC - TOE
-    
+
     return 0;
 }
