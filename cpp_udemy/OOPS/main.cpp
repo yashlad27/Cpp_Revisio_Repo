da39045be17fc12509e09edbff872cc7d82a97c6
Yash Lad <yashlad727@gmail.com>
2022-09-02 16:54:42 +0530

    adding a print fun

--- cpp_udemy/OOPS/main.cpp
+++ cpp_udemy/OOPS/main.cpp
@@ -30,6 +30,8 @@ class House {
         string color;
 };
 
+void print_House_data(const House& house); // increasing reuseability of code
+
 int main(){
     House my_house;
     House your_house;
@@ -42,8 +44,21 @@ int main(){
     your_house.setNumWindows(10);
     your_house.setColor("blue");
 
+    cout<<"----------|X|---------"<<endl;
+
     cout<<"My House is "<<my_house.getColor()<<" and has "<<my_house.getNumStories()<< " stories and "<< my_house.getNumWindows()<< " windows."<<endl;
 
     cout<<"Your House is "<<your_house.getColor()<<" and has "<<your_house.getNumStories()<< " stories and "<< your_house.getNumWindows()<< " windows."<<endl;
+
+    cout<<"----------|X|---------"<<endl;
+
+    print_House_data(my_house);
+    print_House_data(your_house);
+
+    cout<<"----------|X|---------"<<endl;
     return 0;
+}
+
+void print_House_data(const House& house){
+    cout<<"The House is "<<house.getColor()<<" and has "<<house.getNumStories()<<" stories and "<< house.getNumWindows()<< " windows."<<endl;
 }
