15358e3ffec2679fdd72e03d98405db42f141557
Yash Lad <yashlad727@gmail.com>
2022-09-26 14:30:05 +0530

    initial commit 🚀

--- cpp_udemy/Constants/constants.cpp
+++ cpp_udemy/Constants/constants.cpp
@@ -23,11 +23,35 @@ int main()
     return 0;
 }
 
+// neither is a constant ( both can be changed )
 void noConst(){
+    cout<<"In noConst"<<endl;
+
+    int* intPtr = new int(50);
+
+    cout<<"\tOriginal Value: "<<*intPtr<<endl;
+
+    *intPtr = 100;
+    cout<<"\tChange Data"<<*intPtr<<endl;
+
+    delete intPtr; // delete current dynamic int
+
+    intPtr = new int(125);
+    cout<<"\t new Integer entirely: "<< *intPtr << endl;
 
 }
 
+// pointer is constant (cannot be changed)
+// data is not constant (can be changed!)
 void cp2ncd(){
+    cout<<"In cp2ncd: "<<endl;
+
+    int* intPtr = new int(100);
+
+    cout<<"\t orig value: "<<*intPtr<<endl;
+
+    *intPtr = 250;
+    cout<<"\tNew integer value:"<<*intPtr<<endl;
 
 }
 
