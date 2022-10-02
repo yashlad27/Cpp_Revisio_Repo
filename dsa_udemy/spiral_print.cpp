d564a4c1c8fa92fed947e12adbf653f383aefeb0
Yash Lad <yashlad727@gmail.com>
2022-10-02 22:35:56 +0530

    initial commit 🚀

---
+++ dsa_udemy/spiral_print.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+void sprial_print(int a[], int n, int m){
+
+    int startRow = 0;
+    int endRow = n-1;
+    int startCol = 0;
+    int endCol = m-1;
+
+    while(true){
+        
+    }
+}
+
+int main(){
+
+    int spiral[100][100];
+    
+    int n,m; // n,m=4
+    cin>>n>>m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>spiral[i][j];
+        }
+    }
+
+    return 0;
+}
